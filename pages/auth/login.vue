b6c834c33e7978e065f6ee724dc0559d8a3df5f8
opeyemidy <opeyemidy@gmail.com>
2021-02-15 11:49:22 +0100

    validate login and forget password page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -5,38 +5,57 @@
       Welcome back! Please login to your <br />
       account.
     </p>
-    <v-form>
-      <div>
-        <label for="email">Email</label>
-        <v-text-field
-          v-model="first"
-          outlined
-          placeholder="example@company.com"
-        ></v-text-field>
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <v-text-field
-          v-model="first"
-          type="password"
-          placeholder="***********"
-          outlined
-        ></v-text-field>
-      </div>
-      <div class="d-flex align-center mb-3">
-        <v-checkbox
-          label="Remember me"
-          class="ma-0 rm-check-m"
-          color="primary"
-          hide-details
-        ></v-checkbox>
-        <v-spacer></v-spacer>
-        <nuxt-link class="ma-0 forget-password" to="/auth/forgetPassword"
-          >Forget Password?</nuxt-link
-        >
-      </div>
-      <v-btn color="primary" large type="submit" block>Login</v-btn>
-    </v-form>
+    <ValidationObserver ref="loginForm">
+      <v-form @submit.prevent="onSubmit">
+        <div class="mb-5">
+          <ValidationProvider
+            v-slot="{ errors }"
+            name="Email"
+            rules="required|email"
+          >
+            <label for="email">Email</label>
+            <v-text-field
+              v-model="email"
+              type="email"
+              hide-details
+              outlined
+              placeholder="example@company.com"
+            ></v-text-field>
+            <small class="red--text">{{ errors[0] }}</small>
+          </ValidationProvider>
+        </div>
+        <div class="mb-5">
+          <ValidationProvider
+            v-slot="{ errors }"
+            name="Password"
+            rules="required"
+          >
+            <label for="password">Password</label>
+            <v-text-field
+              v-model="password"
+              type="password"
+              hide-details
+              placeholder="***********"
+              outlined
+            ></v-text-field>
+            <small class="red--text">{{ errors[0] }}</small>
+          </ValidationProvider>
+        </div>
+        <div class="d-flex align-center mb-3">
+          <v-checkbox
+            label="Remember me"
+            class="ma-0 rm-check-m"
+            color="primary"
+            hide-details
+          ></v-checkbox>
+          <v-spacer></v-spacer>
+          <nuxt-link class="ma-0 forget-password" to="/auth/forgetPassword"
+            >Forget Password?</nuxt-link
+          >
+        </div>
+        <v-btn color="primary" large type="submit" block>Login</v-btn>
+      </v-form>
+    </ValidationObserver>
     <p class="ma-0 mt-5 mr-2">
       Already have an account?
       <nuxt-link class="forget-password" to="/auth/signup"
@@ -47,12 +66,39 @@
 </template>
 
 <script>
+import { ValidationObserver, ValidationProvider } from 'vee-validate'
 export default {
   layout: 'auth',
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
   data() {
     return {
-      first: '',
+      email: '',
+      password: '',
     }
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.loginForm.validate().then((success) => {
+        if (!success) {
+          return
+        }
+
+        console.log('Form has been submitted!')
+        // this.awaitVerify = true
+        // this.$store.commit('IN_PROGRESS_ACTIVE')
+
+        // Resetting Values
+        this.email = this.password = ''
+
+        // Wait until the models are updated in the UI
+        // this.$nextTick(() => {
+        this.$refs.loginForm.reset()
+        // })
+      })
+    },
   },
 }
 </script>
